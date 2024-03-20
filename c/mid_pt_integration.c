2858553a9cf5016e8c7dde6d0ef617923b6e4d57
mnh <4444574+maruthinh@users.noreply.github.com>
2024-03-20 17:06:53 +0530

    Update mid_pt_integration.c

--- c/mid_pt_integration.c
+++ c/mid_pt_integration.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
     // serial version
     // pi = 0.0;
-    // for (int i = rank; i < n; i++)
+    // for (int i = 0; i < n; i++)
     // {
     //     lim1 = i * dx;
     //     lim2 = lim1 + dx;
